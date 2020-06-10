cf812c7a7bc202baa365b4dcaddd88b3e13fd7d0
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-10 03:10:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -30,22 +30,23 @@ programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
-	programmer_hash[:dennis_ritchies] [:language][0]
+	programmer_hash = 
 	programmer_hash =
  		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+  programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
