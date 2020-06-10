85cd16d029e1e605678bbd48854eb65765120381
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-10 03:42:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -52,7 +52,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-programmer_hash = 
+programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
