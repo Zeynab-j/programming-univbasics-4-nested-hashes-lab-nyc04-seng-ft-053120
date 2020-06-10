2d7500cf93e530a26dd27479ad3ec4015be4f855
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-10 03:15:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,21 +56,15 @@ def adding_matz
 
 # return the entire updated hash
 
-programmer_hash =
-programmer_hash =
-	{
-			:grace_hopper => {
-				:known_for => "COBOL",
-				:languages => ["COBOL", "FORTRAN"]
-			},
-			:alan_kay => {
-				:known_for => "Object Orientation",
-				:languages => ["Smalltalk", "LISP"]
-			},
-			:dennis_ritchie => {
-				:known_for => "Unix",
-				:languages => ["C"]
+programmer_hash = {grace_hopper:"COBOL,FORTRAN", alan_kay:"Smalltalk,LISP", dennis_ritchie:"Unix,C"}
 
+
+   programmer_hash:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]]
+
+   puts programmer_hash
+   }
 end
 
 def changing_alan
