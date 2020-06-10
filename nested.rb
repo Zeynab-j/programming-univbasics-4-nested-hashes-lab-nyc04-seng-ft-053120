ea58beaf2740cddda225b4db79e46b82d4bbaaf7
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-10 03:19:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,11 +58,10 @@ def adding_matz
 
 programmer_hash:yukihiro_matsumoto => {
  :known_for => "Ruby",
- :languages => ["LISP", "C"]]
+ :languages => ["LISP", "C"]
 
  puts programmer_hash
- }
-}
+ 
 end
 
 def changing_alan
